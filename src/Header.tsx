6d8b702d4ee0ef4c2bd097b90dde60cbb5543755
Joel Kram <joelnskram@gmail.com>
2025-08-04 14:54:32 +0100

    add: new font

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 export default function Header() {
   return (
     <div className="container p-2">
-      <div className="text mb-2 w-[800px] text-black text-[11pt] font-['Neue'] tracking-[-0.3px]">
+      <div className="text mb-2 w-[800px] text-black text-[11pt] font-sixtyfour tracking-[-0.3px]">
         <Link to="/">
           <h1 className="underline text-[14pt]">Joel Kram</h1>
         </Link>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// Your config should look like this:
+const config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sixtyfour: ['Sixtyfour', 'sans-serif'],
+      },
+    },
+  },
+  
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [],
+};
+export default config;
